4dc7f09ed9ce01bfbb3c062827352a2c51dd4e60
pavelBabayan <babayan.pavel@gmail.com>
2019-08-14 10:29:23 -0400

    first commit to feature branch

--- src/test/java/LoginTest/Logintest.java
+++ src/test/java/LoginTest/Logintest.java
@@ -3,16 +3,28 @@ package LoginTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Logintest {
 
     WebDriver driver;
 
-    @Test
-    public void test1(){
+    @BeforeMethod
+    public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test1(){
         driver.get("https://www.google.com");
+    }
+
+    @AfterMethod
+    public void closing() throws InterruptedException {
+        driver.quit();
+
     }
 }
