eec05ed4e25a4374a4630390d13abcf6fdec2ec7
pavelBabayan <babayan.pavel@gmail.com>
2019-08-14 10:42:12 -0400

    added new test

--- src/test/java/LoginTest/Logintest.java
+++ src/test/java/LoginTest/Logintest.java
@@ -2,7 +2,9 @@ package LoginTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,6 +22,12 @@ public class Logintest {
     @Test
     public void test1(){
         driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void test2(){
+        driver.get("https://www.google.com");
+        Assert.assertEquals(driver.getTitle(),"Google");
     }
 
     @AfterMethod
